f5288b43c9cac771a97c72c7138a831c73f21a0b
Nedimm23 <dzibra_23@hotmail.com>
2020-08-05 14:24:17 +0200

    added image fade

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+// $(document).ready(function () {
+//   $("#cf7_controls").on("click", "span", function () {
+//     $("#cf7 img").removeClass("opaque");
+
+//     var newImage = $(this).index();
+
+//     $("#cf7 img").eq(newImage).addClass("opaque");
+
+//     $("#cf7_controls span").removeClass("selected");
+//     $(this).addClass("selected");
+//   });
+// });
 $(document).ready(function () {
-  $("#cf7_controls").on("click", "span", function () {
-    $("#cf7 img").removeClass("opaque");
-
-    var newImage = $(this).index();
-
-    $("#cf7 img").eq(newImage).addClass("opaque");
-
-    $("#cf7_controls span").removeClass("selected");
-    $(this).addClass("selected");
+  $("img").click(function (e) {
+    var newclass = $(this).attr("id");
+    var oldclass = $("#full-size").attr("class");
+    $("#full-size").fadeOut(function () {
+      $("#full-size").removeClass(oldclass).addClass(newclass).fadeIn("show");
+    });
   });
 });
