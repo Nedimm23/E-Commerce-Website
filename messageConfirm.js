2588eac03f3c22125d2299d25985420021ad7908
Nedimm23 <dzibra_23@hotmail.com>
2020-08-10 11:54:15 +0200

    some changes and corections

--- messageConfirm.js
+++ messageConfirm.js
@@ -1,7 +1,7 @@
 const messageConfirm = () => {
   let message;
   let option = confirm("Are you sure?");
-  if (!option === true) {
+  if (option === true) {
     message =
       "Thank you for your order, your order will be processed within the next 48 hours!";
     window.location.href = "/";
